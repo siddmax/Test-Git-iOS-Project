97bff5ba56edc406e835dea1432d76e5a397fa60
Saurav Sharma <Neil@sauravsrmasmbp2.att.net>
2014-09-11 13:18:53 -0700

    added a test class

---
+++ SSTest.h
@@ -0,0 +1,13 @@
+//
+//  SSTest.h
+//  Git Test
+//
+//  Created by Saurav Sharma on 9/11/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSTest : NSObject
+
+@end
---
+++ SSTest.m
@@ -0,0 +1,13 @@
+//
+//  SSTest.m
+//  Git Test
+//
+//  Created by Saurav Sharma on 9/11/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import "SSTest.h"
+
+@implementation SSTest
+
+@end
